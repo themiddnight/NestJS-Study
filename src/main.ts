d8c98f5adb16aef2f619258e213932ba3a74682a
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-01-08 10:15:50 +0700

    Update package.json and main.ts, add ejs dependency and set view engine

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 const port = 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setGlobalPrefix('api');
   app.enableCors();
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
 
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
