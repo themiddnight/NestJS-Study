dd182ff097d124d2c55bf233140e46af8077d507
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-01-18 23:15:36 +0700

    Refactor ProductsService to include pagination and error handling

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,98 +3,142 @@ import {
   BadRequestException,
   NotFoundException,
 } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
 import { Op } from 'sequelize';
 
-import { CreateProductDto } from './dto/create-product.dto';
+import {
+  CreateProductDto,
+  ResponseProductsDto,
+  ResponseProductDto,
+  DeleteProductDto,
+} from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './models/product.model';
 import { Category } from '../category/models/category.model';
 
 @Injectable()
 export class ProductsService {
-  async findAll(): Promise<CreateProductDto[]> {
-    return await Product.findAll({
-      include: { model: Category, attributes: ['name'] },
-    });
+  base_url: any;
+  port: any;
+  api_path: any;
+
+  constructor(private configService: ConfigService) {
+    this.base_url = this.configService.get('base_url');
+    this.port = this.configService.get('port');
+    this.api_path = this.configService.get('api_path');
   }
 
-  async findOne(id: number): Promise<CreateProductDto> {
+  async findAll(
+    name: string,
+    page: number,
+    limit: number,
+  ): Promise<ResponseProductsDto> {
+    const nameParam = name || '';
+    const pageParam = page || 1;
+    const limitParam = limit || 10;
+    const productCount = await Product.count({
+      where: { name: { [Op.like]: `%${nameParam}%` } },
+    });
+    const totalPages = Math.ceil(productCount / limitParam);
+    const previousPage = pageParam - 1 < 1 ? 1 : pageParam - 1;
+    const nextPage = pageParam + 1 > totalPages ? totalPages : pageParam + 1;
+    const result = await Product.findAll({
+      where: { name: { [Op.like]: `%${nameParam}%` } },
+      offset: (pageParam - 1) * limitParam,
+      limit: limitParam,
+      include: { model: Category, attributes: ['id', 'name'] },
+    });
+    if (result.length === 0) {
+      throw new NotFoundException({
+        message: 'Could not find products.',
+        data: [],
+      });
+    }
+    return {
+      message: 'Success',
+      current_page: pageParam,
+      total_pages: Math.ceil(productCount / limitParam),
+      previous: `${this.base_url}:${this.port}/${this.api_path}/products?name=${nameParam}&page=${previousPage}&limit=${limitParam}`,
+      next: `${this.base_url}:${this.port}/${this.api_path}/products?name=${nameParam}&page=${nextPage}&limit=${limitParam}`,
+      data: result,
+    };
+  }
+
+  async findOne(id: number): Promise<ResponseProductDto> {
     const result = await Product.findOne({
       where: { id: id },
-      include: { model: Category, attributes: ['name'] },
+      include: { model: Category, attributes: ['id', 'name'] },
     });
     if (!result) {
-      throw new NotFoundException('Could not find product.');
+      throw new NotFoundException({
+        message: `Could not find product with id ${id}.`,
+        data: {},
+      });
     }
-    return result;
+    return {
+      message: 'Success',
+      data: result,
+    };
   }
 
-  async findName(name: string): Promise<CreateProductDto[]> {
-    const result = await Product.findAll({
-      where: { name: { [Op.like]: `%${name}%` } },
-      include: { model: Category, attributes: ['name'] },
-    });
-    if (!result) {
-      throw new NotFoundException('Could not find product.');
-    }
-    return result;
-  }
-
-  async findPage(page: number, limit: number): Promise<CreateProductDto[]> {
-    const result = await Product.findAll({
-      offset: (page - 1) * limit,
-      limit: limit,
-      include: { model: Category, attributes: ['name'] },
-    });
-    if (!result) {
-      throw new NotFoundException('Could not find product.');
-    }
-    return result;
-  }
-
-  async create(createProductDto: CreateProductDto): Promise<CreateProductDto> {
+  async create(
+    createProductDto: CreateProductDto,
+  ): Promise<ResponseProductDto> {
     try {
-      return await Product.create(createProductDto);
+      const result = await Product.create(createProductDto);
+      return {
+        message: `Product with id ${result.id} has been created.`,
+        data: result,
+      };
     } catch (error) {
-      throw new BadRequestException(error.message);
-    }
-  }
-
-  async bulkCreate(
-    createProductDto: CreateProductDto[],
-  ): Promise<CreateProductDto[]> {
-    try {
-      return await Product.bulkCreate(createProductDto);
-    } catch (error) {
-      throw new BadRequestException(error.message);
+      throw new BadRequestException({
+        message: error.message,
+        data: {},
+      });
     }
   }
 
   async update(
     id: number,
     updateProductDto: UpdateProductDto,
-  ): Promise<CreateProductDto> {
+  ): Promise<ResponseProductDto> {
     const product = await Product.findOne({ where: { id: id } });
     if (!product) {
-      throw new NotFoundException('Could not find product.');
+      throw new NotFoundException({
+        message: `Could not find product with id ${id}.`,
+      });
     }
     product.name = updateProductDto.name || product.name;
     product.price = updateProductDto.price || product.price;
     product.cat_id = updateProductDto.cat_id || product.cat_id;
     try {
-      return await product.save();
+      const result = await product.save();
+      return {
+        message: `Product with id ${id} has been updated.`,
+        data: result,
+      };
     } catch (error) {
-      throw new BadRequestException(error.message);
+      throw new BadRequestException({
+        message: error.message,
+      });
     }
   }
 
-  async remove(id: number): Promise<CreateProductDto> {
-    const product = await Product.findOne({ where: { id: id } });
-    if (!product) {
-      throw new NotFoundException('Could not find product.');
+  async remove(id: number): Promise<DeleteProductDto> {
+    try {
+      const result = await Product.destroy({ where: { id: id } });
+      if (result === 0) {
+        throw new NotFoundException({
+          message: `Could not find product with id ${id}.`,
+        });
+      }
+      return {
+        message: `Product with id ${id} has been deleted.`,
+      };
+    } catch (error) {
+      throw new BadRequestException({
+        message: error.message,
+      });
     }
-    product.destroy();
-    return product;
   }
 }
