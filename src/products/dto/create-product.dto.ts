64a2af055d95d4e18f209c1d8a8c7ba38642a7dc
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-01-18 23:16:19 +0700

    Add Category import and update DTOs

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,5 @@
 import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
+import { Category } from '../../category/models/category.model';
 
 export class CreateProductDto {
   @IsNotEmpty()
@@ -12,4 +13,36 @@ export class CreateProductDto {
   @IsNotEmpty()
   @IsNumberString()
   cat_id: number;
+}
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  cat_id: number;
+  category: Category;
+}
+
+/**
+ * Include message, current_page, total_pages, next, previous, data
+ */
+export class ResponseProductsDto {
+  message: string;
+  current_page: number;
+  total_pages: number;
+  next: string;
+  previous: string;
+  data: Product[];
+}
+
+/**
+ * Include message, data
+ */
+export class ResponseProductDto {
+  message: string;
+  data: Product;
+}
+
+export class DeleteProductDto {
+  message: string;
 }
