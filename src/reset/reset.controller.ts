f16015abe6acf55fe73b15108390b1262b1fd77f
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-01-20 20:49:17 +0700

    Change method to PUT

--- src/reset/reset.controller.ts
+++ src/reset/reset.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Put } from '@nestjs/common';
 import { ResetService } from './reset.service';
-import { ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
 
 @Controller('reset')
 export class ResetController {
   constructor(private readonly resetService: ResetService) {}
 
-  @Get()
+  @Put()
   @ApiOperation({ summary: 'Reset database' })
-  @ApiQuery({ name: 'reset', required: true })
-  reset(@Query('reset') reset: boolean) {
+  @ApiBody({ type: Boolean })
+  reset(@Body('reset') reset: boolean) {
     return this.resetService.reset(reset);
   }
 }
